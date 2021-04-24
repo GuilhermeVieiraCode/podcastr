fbd72bab8b9a9192718aca71e202fe15506721d3
Guilherme <alves.guilherme1357@gmail.com>
2021-04-24 11:38:25 -0300

    Funcionalidades de playNext e playPrevious

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -14,7 +14,9 @@ export function Player() {
           currentEpisodeIndex, 
           isPlaying, 
           togglePlay,
-          setPlayingState } = useContext(PlayerContext)
+          setPlayingState,
+          playNext,
+          playPrevious } = useContext(PlayerContext)
 
   useEffect(() => {
     if(!audioRef.current) {
@@ -89,7 +91,7 @@ export function Player() {
           <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar"/>
           </button>
-          <button type="button" disabled={!episode}>
+          <button type="button" disabled={!episode} onClick={() => playPrevious()}>
             <img src="play-previous.svg" alt="Tocar anterior"/>
           </button>
           <button type="button" 
@@ -104,7 +106,7 @@ export function Player() {
             ) }
             
           </button>
-          <button type="button" disabled={!episode}>
+          <button type="button" disabled={!episode} onClick={() => playNext()}>
             <img src="/play-next.svg" alt="Tocar próxima"/>
           </button>
           <button type="button" disabled={!episode}>
